ddceddf45492c16d81bc2298db89076a11784a05
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2016-05-11 12:55:23 -0700

    fix some bug

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   staticPath: '/Users/joe/Codes/spsites/main/src/main/webapp/static',
-  proxy: 'https://manage.symphonycommerce.com',
-  urlRewriteRule: [{
+  proxy: 'https://release191-partner.symphonycommerce.com',
+  urlRewriteRules: [{
     match: /static\/templates\/partner\/compose\/directives/g,
     replace: '/templates/partner/compose/directives'
   }],
-  htmlRewriteRule: null
+  htmlRewriteRules: null
 };
--- url-rewrite.js
+++ url-rewrite.js
@@ -1,8 +1,8 @@
-var urlRewriteRule = require('./config').urlRewriteRule || [];
+var urlRewriteRules = require('./config').urlRewriteRules || [];
 
 module.exports = function(req, res, next) {
   var url = req.url;
-  urlRewriteRule.forEach(function(rule) {
+  urlRewriteRules.forEach(function(rule) {
     url = url.replace(rule.match, rule.replace);
   });
   req.url = url;
