0c56f03fbd46e88dc787fc32352001e47dde1470
Zong Chen <zong@symphonycommerce.com>
2016-07-20 17:10:10 -0700

    Take release-env into account

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Using browser-sync to debug production issues easily.
 
 You can configure the proxy url and your workspace in `config.js`,
 then you are good to go.
-Note: pod and production are both supported by default now.
+Note: pod, release, and production are both supported by default now.
 
 Command:
 ```bash
--- index.js
+++ index.js
@@ -2,19 +2,42 @@ var bs = require("browser-sync").create();
 var config = require('./config');
 var path = require('path');
 
-var isPod = /\w+-(partner|sites)\.symphonycommerce\.com/.test(config.proxy);
+var env = 'production';
+if (/\w+-(partner|sites)\.symphonycommerce\.com/.test(config.proxy)) {
+	env = 'pod';
+} else if ( /\w+-release\.symphonycommerce\.com/.test(config.proxy)) {
+	env = 'release';
+}
 var rewriteFn = function(req, res, match, match1) {
   var output = '/scripts/dist/' + match1 + '.min.js';
   if (config.debug) console.log(match + ' replaced to ' + output);
   return output;
 };
-var defaultHtmlRewriteRules = isPod ? [{
-  match: /\/\/symphony-staging\.s3\.amazonaws\.com\/\w+\/bundle\/[\d\.\w-]+\/js\/(lego|sites|compose|manage)(\.gz)?\.js/gi,
-  fn: rewriteFn
-}] : [{
-  match: /\/\/d3t9cosyzvu4nc.cloudfront.net\/prod\/bundle\/[\d\.]+\/js\/(lego|sites|compose|manage)(\.gz)?\.js/gi,
-  fn: rewriteFn
-}];
+
+var defaultHtmlRewriteRules;
+switch(env) {
+	case 'pod':
+		defaultHtmlRewriteRules = [{
+			match: /\/\/symphony-staging\.s3\.amazonaws\.com\/\w+\/bundle\/[\d\.\w-]+\/js\/(lego|sites|compose|manage)(\.gz)?\.js/gi,
+			fn: rewriteFn
+		}];
+		break;
+	case 'release':
+		defaultHtmlRewriteRules = [{
+			match: /\/\/d2g9u4nhe8bb6x.cloudfront.net\/release\/bundle\/[\d\.\w-]+\/js\/(lego|sites|compose|manage)(\.gz)?\.js/gi,
+			fn: rewriteFn
+		}];
+		break;
+	case 'production':
+		defaultHtmlRewriteRules = [{
+			match: /\/\/d3t9cosyzvu4nc.cloudfront.net\/prod\/bundle\/[\d\.]+\/js\/(lego|sites|compose|manage)(\.gz)?\.js/gi,
+			fn: rewriteFn
+		}];
+		break;
+	default:
+		console.error('Unrecognized `env` value:', env);
+}
+
 var rewriteRules = config.htmlRewriteRules || defaultHtmlRewriteRules;
 
 bs.init({
